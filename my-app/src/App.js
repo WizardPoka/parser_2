d7bab40797d141887ac90d1786497a19874ed6da
WizarPoka <Vasya-nve12401@yandex.ru>
2023-11-28 16:04:25 +0300

    вернул все как есть

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// =================================================================================================
 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// =================================================================================================
 function App() {
+  const [groups, setGroups] = useState([]);
+  const [selectedGroup, setSelectedGroup] = useState(null);
+  const [groupSchedule, setGroupSchedule] = useState([]);
+
+// =================================================================================================
+
+  useEffect(() => {
+    const fetchGroups = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/get_groups');
+        setGroups(response.data);
+      } catch (error) {
+        console.error('Error fetching groups:', error);
+      }
+    };
+
+    fetchGroups();
+  }, []);
+
+// =================================================================================================
+
+const fetchGroupSchedule = async (groupName) => {
+  try {
+    const response = await axios.post('http://localhost:5000/get_schedule', { group_name: groupName });
+    console.log(response.data); // Оставьте эту строку для отладки
+    setGroupSchedule(Array.isArray(response.data) ? response.data : []);
+  } catch (error) {
+    console.error('Error fetching schedule:', error);
+  }
+};
+
+// =================================================================================================
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Group Schedule</h1>
+      <div>
+        <h2>Groups:</h2>
+        <ul>
+          {groups.map((group, index) => (
+            <li key={index} onClick={() => { setSelectedGroup(group); fetchGroupSchedule(group); }}>
+              {group}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {selectedGroup && (
+        <div>
+          <h2>Schedule for {selectedGroup}:</h2>
+          <ul>
+            {groupSchedule.map(({ День, Урок, [selectedGroup]: selectedGroupValue = '' }, index) => (
+              <li key={index}>{`${День} - ${Урок}: ${selectedGroupValue || 'No lesson'}`}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
 
+// =================================================================================================
+
 export default App;
+
+// =================================================================================================
---
+++ server.py
@@ -0,0 +1,44 @@
+# =================================================================================================
+
+# server.py
+import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pandas as pd
+
+# =================================================================================================
+
+app = Flask(__name__)
+CORS(app, resources={r"/get_*": {"origins": "http://localhost:3000"}})
+
+# =================================================================================================
+
+df = pd.read_excel(r'c:\new_VKR_parser\1.xlsx')
+
+# =================================================================================================
+
+@app.route('/get_groups', methods=['GET'])
+def get_groups():
+    groups = df.columns[2:].tolist()
+
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Access-Control-Allow-Origin': '*'  # Добавляем заголовок для CORS, чтобы разрешить запросы из любых источников
+    }
+
+    return jsonify(groups), 200, headers
+
+@app.route('/get_schedule', methods=['POST'])
+def get_schedule():
+    group_name = request.json['group_name']
+    group_schedule = df[df[group_name].notnull()][['День', 'Урок', group_name]].to_dict(orient='records')
+    print(group_schedule)
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Access-Control-Allow-Origin': '*'  # Добавляем заголовок для CORS, чтобы разрешить запросы из любых источников
+    }
+
+    return (json.dumps(group_schedule, ensure_ascii=False), 200, headers)
+
+if __name__ == '__main__':
+    app.run(debug=True)

fcceecc3227378b43b47b8619ef68cfa5c98f7a4
WizarPoka <Vasya-nve12401@yandex.ru>
2024-03-10 01:09:35 +0300

    дни недели парсятся идеально!

--- my-app/src/components/Testapp.jsx
+++ my-app/src/components/Testapp.jsx
@@ -1,6 +1,8 @@
+// Testapp.jsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Testapp.css';
+import styles from './Testapp.module.css';
+
 function Testapp() {
   const [groups, setGroups] = useState([]);
   const [selectedGroup, setSelectedGroup] = useState(null);
@@ -24,99 +26,84 @@ function Testapp() {
     fetchGroups();
   }, []);
 
-// =================================================================================================
-
-const fetchGroupSchedule = async (groupName) => {
-  try {
-    const response = await axios.post('http://localhost:5000/get_schedule', { group_name: groupName });
-    console.log('Response data:', response.data);
-    setGroupSchedule(response.data || []);
-  } catch (error) {
-    console.error('Error fetching schedule:', error);
-  }
-};
-
-
-
+  const fetchGroupSchedule = async (groupName) => {
+    try {
+      const response = await axios.post('http://localhost:5000/get_schedule', { group_name: groupName });
+      console.log('Response data:', response.data);
   
-// =================================================================================================
+      const processedData = processScheduleData(response.data);
+  
+      setGroupSchedule(processedData || []);
+    } catch (error) {
+      console.error('Error fetching schedule:', error);
+    }
+  };
+  
+  const processScheduleData = (data) => {
+    let currentDay = null;
+  
+    return data.map(item => {
+      if (item.День) {
+        currentDay = item.День;
+      } else if (currentDay) {
+        item.День = currentDay;
+      }
+      return item;
+    });
+  };
+  
+  
 
-
+  // const fetchGroupSchedule = async (groupName) => {
+  //   try {
+  //     const response = await axios.post('http://localhost:5000/get_schedule', { group_name: groupName });
+  //     console.log('Response data:', response.data);
+  //     setGroupSchedule(response.data || []);
+  //   } catch (error) {
+  //     console.error('Error fetching schedule:', error);
+  //   }
+  // };
 
   return (
-    <div>
-
-{/* <section>
-      <h1>Group Schedule</h1>
-      
-      
-        <h2>Groups:</h2>
+    <div className={styles.container}>
+      <div className={styles.groupList}>
+        <h1>Groups:</h1>
         <ul>
-          <li>
-            <p>
-              {groups.map((group, index) => (
-                <li key={index} onClick={() => { setSelectedGroup(group); fetchGroupSchedule(group); }}>
-                  {group}
-                </li>
-              ))}
-            </p>
-          </li>
-        </ul>
-      </section> */}
-
-      <h1>Groups:</h1>
-      <ul>
-        {groups.slice(0, 3).map((group, index) => (
-          <li key={index} onClick={() => { setSelectedGroup(group); fetchGroupSchedule(group); }}>
-            {group}
-          </li>
-        ))}
-      </ul>
-
-     
-  <div>
-    <table>
-            <thead>
-              <tr>
-                <th>День</th>
-                <th>Урок</th>
-                <th>{selectedGroup}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {groupSchedule.map((scheduleItem, index) => (
-                <tr key={index}>
-                  <td>{scheduleItem['День'] || 'No data'}</td>
-                  <td>{scheduleItem['Урок'] || 'No data'}</td>
-                  <td>{scheduleItem[selectedGroup] || 'No data'}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-  </div>
-
-    {/* <h2>Schedule for {selectedGroup}:</h2>
-        <ul>
-          {groupSchedule.map((scheduleItem, index) => (
-            <li key={index} >
-              <strong>Dictionary {index + 1}:</strong>
-              <ul>
-              {Object.entries(scheduleItem).map(([key, value]) => (
-                    <li key={key}>
-                      <strong>{key}:</strong> {value}
-                    </li>
-                  ))}
-                </ul>
+          {groups.slice(0, 13).map((group, index) => (
+            <li key={index} onClick={() => { setSelectedGroup(group); fetchGroupSchedule(group); }} className={styles.listItem}>
+              {group}
             </li>
           ))}
-        </ul> */}
+        </ul>
+      </div>
 
-
-
+      <div className={styles.scheduleTable}>
+        {selectedGroup && (
+          <div>
+            <h2>Schedule for {selectedGroup}:</h2>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>День</th>
+                  <th>Урок</th>
+                  <th>{selectedGroup}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {groupSchedule.map((scheduleItem, index) => (
+                  <tr key={index}>
+                    <td>{scheduleItem['День'] || 'No data'}</td>
+                    <td>{scheduleItem['Урок'] || 'No data'}</td>
+                    <td>{scheduleItem[selectedGroup] || 'No data'}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
 export default Testapp;
-
-
